7744a3a6a0bd1a23cedc712bdf0cccc085ec3c62
Mateusz Samek <samekmat@gmail.com>
2024-04-28 19:29:04 +0200

    feat: add vcard model

--- vcards/models.py
+++ vcards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class VCard(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField()
+    image = models.ImageField(upload_to='images/')
+    unique_url = models.CharField(max_length=100, unique=True)
+    unique_qr_code = models.ImageField(upload_to='qr_codes/', unique=True)
+    unique_vcard_file = models.FileField(upload_to='vcard_files/', unique=True)
+
