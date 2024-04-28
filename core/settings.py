e434d3aaaaa89c74639e3e3a48d67a673b8c2f09
Mateusz Samek <samekmat@gmail.com>
2024-04-28 19:22:58 +0200

    refactor: rename app from: business_card to: vcards

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'business_card.apps.BusinessCardConfig',
+    'vcard.apps.BusinessCardConfig',
     'users',
 
     "crispy_forms",
