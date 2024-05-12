3c75215a304e9604c641f52e40333802e20bdab5
Mateusz Samek <samekmat@gmail.com>
2024-05-12 18:51:46 +0200

    feat: add logout_view and adjust urls

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView
+from django.contrib.auth.views import LoginView
+
+from .views import RegisterView, logout_view
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', logout_view, name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -7,3 +9,8 @@ class RegisterView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('users:login')
     template_name = 'users/register.html'
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('users:login')
