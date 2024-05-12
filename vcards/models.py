17524f345aaa615f145f92aa60017cc46a964d85
Mateusz Samek <samekmat@gmail.com>
2024-05-12 18:53:39 +0200

    feat: add vcard model

--- vcards/models.py
+++ vcards/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class VCard(models.Model):
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    company_name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField()
-    image = models.ImageField(upload_to='images/')
-    unique_url = models.CharField(max_length=100, unique=True)
-    unique_qr_code = models.ImageField(upload_to='qr_codes/', unique=True)
-    vcard_file = models.FileField(upload_to='vcard_files/')
-
+    name = models.CharField(max_length=100, help_text="Enter the name.")
+    surname = models.CharField(max_length=100, help_text="Enter the surname.")
+    company_name = models.CharField(max_length=100, help_text="Enter the company name.")
+    phone = models.CharField(max_length=20, help_text="Enter the phone number.")
+    email = models.EmailField(help_text="Enter the email address.")
+    image = models.ImageField(upload_to='images/', help_text="Upload an image.")
+    unique_url = models.CharField(max_length=100, unique=True, help_text="Enter a unique URL.")
+    qr_code = models.ImageField(upload_to='qr_codes/', help_text="Upload a QR code image.")
+    vcard_file = models.FileField(upload_to='vcard_files/', help_text="Upload a vCard file.")
